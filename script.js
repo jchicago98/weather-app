a9f67b46bb50628a1b5dba4ef45254e279d68c9f
Jorge Morin <jmorin1@hawk.iit.edu>
2023-06-30 14:22:38 -0500

    Feature: API can now retrieve live weather data

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function logHello(){
-    console.log("Hello")
+function getWeatherData() {
+    const API_KEY = '3ffefa880cfb4cb59f6183944233006';
+    let location_name = document.getElementById('location_input').value;
+    const URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location_name}`;
+
+    fetch(URL)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // Handle the response data
+        })
+        .catch(error => {
+            console.error('Error:', error); // Handle any errors
+        });
+
 }
