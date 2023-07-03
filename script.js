2568d47a51f8e3b5d46f87067ce470ffce2e2164
Jorge Morin <jmorin1@hawk.iit.edu>
2023-07-03 14:05:16 -0500

    Feature: Users can now see all live weather data for locations they select or type in

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-function getWeatherData() {
-    const city_name = document.getElementById('city_name');
-    const temperature = document.getElementById('temperature');
-    const cloud_percentage = document.getElementById('cloud_percentage');
-    const humidity_percentage = document.getElementById('humidity_percentage');
-    const wind_speed = document.getElementById('wind_speed');
-    const rainfall = document.getElementById('rainfall');
+var location_name = '';
 
+function getWeatherData() {
+    let city_name = document.getElementById('city_name');
+    let temperature = document.getElementById('temperature');
+    let cloud_percentage = document.getElementById('cloud_percentage');
+    let humidity_percentage = document.getElementById('humidity_percentage');
+    let wind_speed = document.getElementById('wind_speed');
+    let rainfall = document.getElementById('rainfall');
+    let user_location_input = document.getElementById('location_input').value
     const API_KEY = '3ffefa880cfb4cb59f6183944233006';
-    let location_name = document.getElementById('location_input').value;
+
+    if (location_name === '') {
+        if (user_location_input === '') {
+            location_name = 'Chicago';
+        }
+        else {
+            location_name = user_location_input;
+        }
+    }
+
+
+
     const URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location_name}`;
 
     fetch(URL)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
             city_name.innerHTML = data.location.name;
-            temperature.innerHTML = data.current.feelslike_f;
+            temperature.innerHTML = data.current.temp_f + "&#176;";
             cloud_percentage.innerHTML = data.current.cloud + "%";
             humidity_percentage.innerHTML = data.current.humidity + "%";
             wind_speed.innerHTML = data.current.wind_mph + " mph";
@@ -25,26 +37,37 @@ function getWeatherData() {
             console.error('Error:', error);
         });
 
+    location_name = '';
+
+}
+
+function updateWeatherHyperLinks(location_input) {
+    location_name = location_input;
+    getWeatherData();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+    getWeatherData();
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+
     const localTimeElement = document.getElementById("local_time");
     const options = {
-      weekday: "long",
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-      hour12: true,
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
     };
-  
+
     function updateLocalTime() {
-      const currentTime = new Date().toLocaleString(undefined, options);
-      localTimeElement.textContent = currentTime;
+        const currentTime = new Date().toLocaleString(undefined, options);
+        localTimeElement.textContent = currentTime;
     }
-  
+
     setInterval(updateLocalTime, 1000);
     updateLocalTime();
-  });
-  
+});
