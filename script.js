27a12a20dffb4c1ccd51eb4b0b39cc3e11ccc61c
Jorge Morin <jmorin1@hawk.iit.edu>
2023-07-03 14:52:57 -0500

    Feature: Background color updates per users local time on browser, updated weather conditions to be dynamic

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ function getWeatherData() {
     let humidity_percentage = document.getElementById('humidity_percentage');
     let wind_speed = document.getElementById('wind_speed');
     let rainfall = document.getElementById('rainfall');
-    let user_location_input = document.getElementById('location_input').value
+    let current_weather_condition = document.getElementById('current_weather_condition');
+    let user_location_input = document.getElementById('location_input').value;
+    let weather_icon_image = document.getElementById('weather_icon_image').src;
     const API_KEY = '3ffefa880cfb4cb59f6183944233006';
 
     if (location_name === '') {
@@ -32,13 +34,14 @@ function getWeatherData() {
             humidity_percentage.innerHTML = data.current.humidity + "%";
             wind_speed.innerHTML = data.current.wind_mph + " mph";
             rainfall.innerHTML = data.current.precip_in + '"';
+            current_weather_condition.innerHTML = data.current.condition.text;
         })
         .catch(error => {
             console.error('Error:', error);
         });
 
     location_name = '';
-
+    updateBackgroundColor();
 }
 
 function updateWeatherHyperLinks(location_input) {
@@ -46,8 +49,28 @@ function updateWeatherHyperLinks(location_input) {
     getWeatherData();
 }
 
+function updateBackgroundColor() {
+    const currentHour = new Date().getHours();
+    const bodyElement = document.body;
+    const card1Element = document.getElementById('left_card');
+
+    if (currentHour >= 6 && currentHour < 12) {
+        bodyElement.style.backgroundImage = 'linear-gradient(#FFD54F, #FFA000)';
+        card1Element.style.backgroundImage = 'linear-gradient(#FFA000, #FFD54F)';
+    }
+    else if (currentHour >= 12 && currentHour < 18) {
+        bodyElement.style.backgroundImage = 'linear-gradient(#81D4FA, #2196F3)';
+        card1Element.style.backgroundImage = 'linear-gradient(#2196F3, #81D4FA)';
+    }
+    else {
+        bodyElement.style.backgroundImage = 'linear-gradient(#9E9E9E, #424242)';
+        card1Element.style.backgroundImage = 'linear-gradient(#424242, #9E9E9E)';
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     getWeatherData();
+    updateBackgroundColor();
 })
 
 document.addEventListener("DOMContentLoaded", function () {
